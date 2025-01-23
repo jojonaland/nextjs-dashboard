4cdd99cd6acade57e6d9ad9b10f8ac6892e8ae99
Jonathan <jonathan@MacBook-Pro-de-Jonathan.local>
2025-01-23 21:17:42 +0100

    NEXTAUTH

--- auth.config.ts
+++ auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
